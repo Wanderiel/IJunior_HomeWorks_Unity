536e7a5c4f9ea9ff7bed216869c169e2dca3c237
Pavel Dymov <dpavel82@yandex.ru>
2024-03-23 20:07:07 +0300

    Задание Счётчик

---
+++ Assets/_Homeworks/06.5_Counter/Scripts/Timer.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace Counter
+{
+    public class Timer : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI _text;
+        [SerializeField] float _speed = 0.5f;
+
+        private float _time;
+        private int _score;
+
+        private Coroutine _coroutine;
+
+        private void Awake()
+        {
+            _score = 0;
+            DisplayScore();
+        }
+
+        public void SwitchTick()
+        {
+            if (_coroutine != null)
+            {
+                StopTick();
+
+                return;
+            }
+
+            _coroutine = StartCoroutine(TickCoroutine());
+        }
+
+        private IEnumerator TickCoroutine()
+        {
+            while (true)
+            {
+                _time += Time.deltaTime;
+                Scoreup();
+                DisplayScore();
+
+                yield return null;
+            }
+        }
+
+        private void StopTick()
+        {
+            StopCoroutine(_coroutine);
+
+            _coroutine = null;
+        }
+
+        private void Scoreup()
+        {
+            if (_time > _speed)
+            {
+                _score++;
+                _time -= _speed;
+            }
+        }
+
+        private void DisplayScore() =>
+            _text.text = _score.ToString();
+    }
+}
